90286a82698e6ea1c7788c075cebd6b4638b6e47
pokon69 <80775597+pokon69@users.noreply.github.com>
2023-03-14 14:48:44 -0400

    Add files via upload

---
+++ SyncDemo2.cpp
@@ -0,0 +1,64 @@
+#include <vector> 
+#include <pthread.h>
+#include <iostream>
+#include <unistd.h>
+
+//Creates mutex lock m
+pthread_mutex_t m; 
+
+//Variable grandTotal is a global var that tracks the added sum of all members in the vector
+//Varaible count tracks which element pos we are in the vector
+int grandTotal, count, n = 5; 
+
+void *calculateSum(void *args){
+    int timer = 0;
+    std::vector<int>* vec = (std::vector<int>*) args;
+    pthread_mutex_lock(&m);
+    while(timer < 200){
+        int temp = grandTotal + vec->at(count);
+        usleep(200);
+        grandTotal = temp;
+        count ++;
+        timer ++;
+    }
+    pthread_mutex_unlock(&m);
+    return NULL;
+}
+
+
+int main(){
+    //Creates vector that holds 1000 ints
+    std::vector <int> vec(1000, 0);
+
+    //FIlls vector with ints from 1-1000 in ascending order
+    for(int i = 0; i < vec.size(); i++){
+        vec.at(i) = i+1;
+    }
+
+    //Creates 5 threads and runs it through method "calculateSum" if there is a error
+    //it prints a error message and exits program
+    int ret;
+    pthread_t thread[n];
+    pthread_mutex_init(&m, 0);
+
+    for(int id = 1; id <= n; id++){
+        ret = pthread_create(&thread[id], NULL, calculateSum, &vec);
+
+        if(ret!=0){
+            std::cout << "Error creating thread: " << ret << std:: endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //Joins all existing threads
+    for(int id = 1; id <= n; id++){
+        pthread_join(thread[id], NULL);
+    }
+
+    pthread_mutex_destroy(&m);
+
+    std::cout << "Grand Total: " << grandTotal << std::endl;
+
+    return 0;
+
+}
